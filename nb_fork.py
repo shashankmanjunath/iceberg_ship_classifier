d09e3d52b040bc5ed1cb387fb494b1c97ccc31b0
manjuns <shashank.manjunath@vanderbilt.edu>
2017-12-18 17:18:13 -0500

    sample model testing

--- nb_fork.py
+++ nb_fork.py
@@ -3,9 +3,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 
 #Load the data.
-train = pd.read_json("../iceberg_ship_classifier/data_train/train.json")
+# train = pd.read_json("../iceberg_ship_classifier/data_train/train.json")
+train = pd.read_json("../icebergClassifier/data_train/train.json")
 # test = pd.read_json("../iceberg_ship_classifier/data_test/test.json")
 
 #Generate the training data
@@ -15,7 +17,6 @@ X_band_2=np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in
 X_train = np.concatenate([X_band_1[:, :, :, np.newaxis], X_band_2[:, :, :, np.newaxis],((X_band_1+X_band_2)/2)[:, :, :, np.newaxis]], axis=-1)
 
 #Import Keras.
-from matplotlib import pyplot
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Input, Flatten, Activation
@@ -89,19 +90,44 @@ callbacks = get_callbacks(filepath=file_path, patience=5)
 target_train=train['is_iceberg']
 X_train_cv, X_valid, y_train_cv, y_valid = train_test_split(X_train, target_train, random_state=1, train_size=0.75)
 
+print('Validating Model...')
+n_split = 5
+kfold = StratifiedKFold(n_splits=n_split, shuffle=True)
+loss = []
+count = 0
 
-gmodel=getModel()
+for train, test in kfold.split(X_train_cv, y_train_cv):
+    print('Run ' + str(count + 1) + ' out of ' + str(n_split))
+    gmodel = getModel()
+    gmodel.fit(X_train_cv[train], y_train_cv[train],
+               epochs=100,
+               batch_size=1,
+               verbose=1,
+               callbacks=callbacks)
+
+    scores = gmodel.evaluate(X_train_cv[test], y_train_cv[test])
+    loss.append(scores[0])
+    count += 1
+
+print("")
+print("Length of scores: " + str(len(loss)))
+
+for i in range(len(loss)):
+    print("Run " + str(i+1) + ": " + str(loss[i]))
+
+print("Loss: " + str(np.mean(loss)), str(np.std(loss)))
+print("")
 
 
-gmodel.fit(X_train_cv, y_train_cv,
-          batch_size=24,
-          epochs=50,
-          verbose=1,
-          validation_data=(X_valid, y_valid),
-          callbacks=callbacks)
-
-gmodel.load_weights(filepath=file_path)
-score = gmodel.evaluate(X_valid, y_valid, verbose=1)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+# gmodel.fit(X_train_cv, y_train_cv,
+#           batch_size=24,
+#           epochs=50,
+#           verbose=1,
+#           validation_data=(X_valid, y_valid),
+#           callbacks=callbacks)
+#
+# gmodel.load_weights(filepath=file_path)
+# score = gmodel.evaluate(X_valid, y_valid, verbose=1)
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
 
