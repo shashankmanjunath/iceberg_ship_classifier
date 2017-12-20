ffc72f2626fa2ec308f3bd0952a550b3f6fb013e
manjuns <shashank.manjunath@vanderbilt.edu>
2017-12-19 22:12:45 -0500

    vgg16 model implemented

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 from loadData import loader
 import pandas as pd
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Input, Dense, Flatten, Dropout, Activation
-from keras.optimizers import Adam
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, MaxPooling2D, Input, Dense, Flatten, Dropout, Activation, GlobalMaxPooling2D
+from keras.optimizers import Adam, SGD
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from sklearn.model_selection import StratifiedKFold
+from keras.applications.vgg16 import VGG16
 
 
 class iceberg_model:
@@ -61,6 +62,30 @@ class iceberg_model:
                            optimizer=opt,
                            metrics=['accuracy'])
 
+    def vgg_model(self):
+        if not self.dataLoader:
+            self.dataLoader = loader(data_path=self.dataPath)
+        trainImg, valImg, trainLabels, valLabels = self.dataLoader.train_test_more_images()
+
+        base_model = VGG16(weights='imagenet', include_top=False,
+                           input_shape=trainImg.shape[1:], classes=1)
+        x = base_model.get_layer('block5_pool').output
+
+        x = GlobalMaxPooling2D()(x)
+        merge_one = Dense(512, activation='relu', name='fc2')(x)
+        merge_one = Dropout(0.3)(merge_one)
+        merge_one = Dense(512, activation='relu', name='fc3')(merge_one)
+        merge_one = Dropout(0.3)(merge_one)
+
+        predictions = Dense(1, activation='sigmoid')(merge_one)
+
+        self.model = Model(input=[base_model.input], output=predictions)
+
+        sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+        self.model.compile(loss='binary_crossentropy',
+                           optimizer=sgd,
+                           metrics=['accuracy'])
+
     def callbacks(self):
         stop = EarlyStopping("val_loss", patience=5, mode="min")
         check = ModelCheckpoint(self.run_weight_name, save_best_only=True)
@@ -75,10 +100,9 @@ class iceberg_model:
 
 
         self.model.fit(trainImg, trainLabels,
-                       batch_size=24,
                        epochs=50,
-                       verbose=1,
                        validation_data=(valImg, valLabels),
+                       verbose=1,
                        callbacks=self.callbacks())
 
     def test_model(self):
@@ -112,7 +136,7 @@ class iceberg_model:
 
         for train_k, test_k in kfold.split(trainImg, trainLabel):
             print('Run ' + str(count + 1) + ' out of ' + str(n_split))
-            self.create_model()
+            self.vgg_model()
 
             self.model.fit(trainImg[train_k], trainLabel[train_k],
                            epochs=50,
@@ -156,6 +180,7 @@ if __name__ == '__main__':
     # data_path = '../icebergClassifier/data_train/train.json'
     x = iceberg_model(data_path)
     # x.train_model()
+    # x.test_model()
     x.kFoldValidation()
     # x.submission('../iceberg_ship_classifier/data_test/test.json')
 
