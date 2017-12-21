33ac47d7865f663fa471683e01989f09b51d938a
manjuns <shashank.manjunath@vanderbilt.edu>
2017-12-20 19:18:34 -0500

    vgg model w/o angles

--- train_vgg.py
+++ train_vgg.py
@@ -29,23 +29,23 @@ class iceberg_model:
                                       rotation_range=10)
 
     def vgg_model(self):
-        input_2 = Input(shape=[1], name="angle")
-        angle_layer = Dense(1, )(input_2)
+        # input_2 = Input(shape=[1], name="angle")
+        # angle_layer = Dense(1, )(input_2)
         base_model = VGG16(weights='imagenet', include_top=False,
                            input_shape=self.dataLoader.X_train.shape[1:], classes=1)
         x = base_model.get_layer('block5_pool').output
 
         x = GlobalMaxPooling2D()(x)
-        merge_one = concatenate([x, angle_layer])
-        merge_one = Dense(512, activation='relu', name='fc2')(merge_one)
+        # merge_one = concatenate([x, angle_layer])
+        merge_one = Dense(512, activation='relu', name='fc2')(x)
         merge_one = Dropout(0.3)(merge_one)
         merge_one = Dense(512, activation='relu', name='fc3')(merge_one)
         merge_one = Dropout(0.3)(merge_one)
 
         predictions = Dense(1, activation='sigmoid')(merge_one)
 
-        model = Model(input=[base_model.input, input_2], output=predictions)
-
+        # model = Model(input=[base_model.input, input_2], output=predictions)
+        model = Model(input=base_model.input, output=predictions)
         sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
         model.compile(loss='binary_crossentropy',
                       optimizer=sgd,
@@ -59,7 +59,8 @@ class iceberg_model:
         while True:
             X1i = genX1.next()
             X2i = genX2.next()
-            yield[X1i[0], X2i[1]], X1i[1]
+            # yield[X1i[0], X2i[1]], X1i[1]
+            yield X1i[0], X1i[1]
 
     def callbacks(self):
         es = EarlyStopping("loss", patience=10, mode="min")
@@ -67,37 +68,9 @@ class iceberg_model:
         return [es, msave]
 
     def kFoldValidation(self):
-        train = pd.read_json("../iceberg_ship_classifier/data_train/train.json")
-        test = pd.read_json("../iceberg_ship_classifier/data_test/test.json")
-        target_train = train['is_iceberg']
-
-        test['inc_angle'] = pd.to_numeric(test['inc_angle'], errors='coerce')
-        train['inc_angle'] = pd.to_numeric(train['inc_angle'], errors='coerce')  # We have only 133 NAs.
-        train['inc_angle'] = train['inc_angle'].fillna(method='pad')
-        X_angle = train['inc_angle']
-        test['inc_angle'] = pd.to_numeric(test['inc_angle'], errors='coerce')
-        X_test_angle = test['inc_angle']
-
-        # Generate the training data
-        X_band_1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train["band_1"]])
-        X_band_2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in train["band_2"]])
-        X_band_3 = (X_band_1 + X_band_2) / 2
-        # X_band_3=np.array([np.full((75, 75), angel).astype(np.float32) for angel in train["inc_angle"]])
-        X_train = np.concatenate([X_band_1[:, :, :, np.newaxis]
-                                     , X_band_2[:, :, :, np.newaxis]
-                                     , X_band_3[:, :, :, np.newaxis]], axis=-1)
-
-        X_band_test_1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test["band_1"]])
-        X_band_test_2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in test["band_2"]])
-        X_band_test_3 = (X_band_test_1 + X_band_test_2) / 2
-        # X_band_test_3=np.array([np.full((75, 75), angel).astype(np.float32) for angel in test["inc_angle"]])
-        X_test = np.concatenate([X_band_test_1[:, :, :, np.newaxis]
-                                    , X_band_test_2[:, :, :, np.newaxis]
-                                    , X_band_test_3[:, :, :, np.newaxis]], axis=-1)
-
-        trainImg = X_train
-        trainAngle = X_angle
-        trainLabel = target_train
+        trainImg = self.dataLoader.X_train
+        trainAngle = self.dataLoader.inc_angle
+        trainLabel = self.dataLoader.labels
 
         n_split = 5
         kfold = StratifiedKFold(n_splits=n_split, shuffle=True, random_state=16)
@@ -117,7 +90,8 @@ class iceberg_model:
                                      verbose=1,
                                      callbacks=callbacks)
 
-            scores = self.model.evaluate([trainImg[test_k], trainAngle[test_k]], trainLabel[test_k])
+            # scores = self.model.evaluate([trainImg[test_k], trainAngle[test_k]], trainLabel[test_k])
+            scores = self.model.evaluate(trainImg[test_k], trainLabel[test_k])
             print(scores)
             loss.append(scores[0])
             count += 1
@@ -130,7 +104,6 @@ class iceberg_model:
 
         print("Loss: " + str(np.mean(loss)), str(np.std(loss)))
         print("")
-        return 0
 
 
 if __name__ == '__main__':
