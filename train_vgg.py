35d6b80e5c3bfb919383019f65b691f6492c944d
manjuns <shashank.manjunath@vanderbilt.edu>
2017-12-28 22:46:42 -0500

    PL changed training parameters

--- train_vgg.py
+++ train_vgg.py
@@ -294,4 +294,4 @@ if __name__ == '__main__':
     # data_path = '../icebergClassifier/data_train/train.json'
     # data_test = '../icebergClassifier/data_test/test.json'
     x = iceberg_model(data_path)
-    x.pseudoLabelTrain(data_test)
+    x.pseudoLabelingValidation(data_test)
